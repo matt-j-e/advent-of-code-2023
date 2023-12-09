fade2c1acd2e592b9dda154bfbfb000a39a5f75f
Matt Edwards <the.matt.edwards@gmail.com>
2023-12-09 12:27:26 +0000

    day 9 part two

--- day9/index.js
+++ day9/index.js
@@ -32,6 +32,16 @@ function findNextVal(arr) {
   return _arrays[0][_arrays[0].length - 1]
 }
 
+function findPrevVal(arr) {
+  const _arrays = [...arr.reverse()]
+  while(_arrays.length > 1) {
+    const bottomLayer = _arrays.shift()
+    const valToAdd = _arrays[0][0] - bottomLayer[0]
+    _arrays[0].unshift(valToAdd)
+  }
+  return _arrays[0][0]
+}
+
 let arrays = []
 let sum = 0
 lines.forEach(line => {
@@ -40,3 +50,12 @@ lines.forEach(line => {
   arrays = []
 })
 console.log('Part One:', sum) // 1789635132
+
+arrays = []
+sum = 0
+lines.forEach(line => {
+  buildArrayOfArrays(line)
+  sum += findPrevVal(arrays)
+  arrays = []
+})
+console.log('Part Two:', sum) // 913
