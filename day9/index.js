c73e4bfd5f9382b7efbe3dbff0af6f4bd97f14ed
Matt Edwards <the.matt.edwards@gmail.com>
2023-12-09 12:17:14 +0000

    day 9 part one

---
+++ day9/index.js
@@ -0,0 +1,42 @@
+const helpers = require('../helpers/helperFunctions');
+
+const input = helpers.loadData(__dirname.split('/').pop(), false).map(line => line.split(' '))
+const lines = input.map(line => line.map(num => parseInt(num)))
+// console.log(lines)
+
+function allZeros(nums) {
+  return nums.every(num => num === 0)
+}
+
+function nextLevel(nums) {
+  const _nums = []
+  for (let i = 0; i < nums.length - 1; i++) {
+    _nums.push(nums[i + 1] - nums[i])
+  }
+  return _nums
+}
+
+function buildArrayOfArrays(nums) {
+  arrays.push(nums)
+  if (allZeros(nums)) return
+  return buildArrayOfArrays(nextLevel(nums))
+}
+
+function findNextVal(arr) {
+  const _arrays = [...arr.reverse()]
+  while (_arrays.length > 1) {
+    const bottomLayer = _arrays.shift()
+    const valToAdd = _arrays[0][_arrays[0].length - 1] + bottomLayer[bottomLayer.length - 1]
+    _arrays[0].push(valToAdd)
+  }
+  return _arrays[0][_arrays[0].length - 1]
+}
+
+let arrays = []
+let sum = 0
+lines.forEach(line => {
+  buildArrayOfArrays(line)
+  sum += findNextVal(arrays)
+  arrays = []
+})
+console.log('Part One:', sum) // 1789635132
