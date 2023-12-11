1a5fff93a15db4b32083ab7814d131dfe8012ea7
Matt Edwards <the.matt.edwards@gmail.com>
2023-12-11 19:49:09 +0000

    day 11 parts 1 & 2

--- day11/index.js
+++ day11/index.js
@@ -1,7 +1,9 @@
 const helpers = require('../helpers/helperFunctions');
 
-const lines = helpers.loadData(__dirname.split('/').pop(), true)
-console.log(lines)
+const lines = helpers.loadData(__dirname.split('/').pop(), false)
+// console.log(lines)
+const emptyC = emptyCols(lines)
+const emptyR = emptyRows(lines)
 
 function emptyCols(rows) {
   const populatedCols = new Set()
@@ -13,6 +15,14 @@ function emptyCols(rows) {
   const result = []
   for (let i = 0; i < rows[0].length; i++) {
     if (!Array.from(populatedCols).includes(i)) result.push(i)
+  }
+  return result
+}
+
+function emptyRows(lines) {
+  const result = []
+  for (let i = 0; i < lines.length; i++) {
+    if (!lines[i].includes('#')) result.push(i)
   }
   return result
 }
@@ -44,19 +54,57 @@ function expandVertically(rows) {
   return result
 }
 
-const space = expandVertically(lines)
-
-let num = 1
-let row = 0
-while (row < space.length) {
-  const arr = space[row].split('')
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === '#') {
-      arr[i] = num
-      num++
+function galaxies(space) {
+  const locations = []
+  for (let r = 0; r < space.length; r++) {
+    for (let c = 0; c < space[r].length; c++) {
+      if (space[r][c] === '#') {
+        const coords = {r, c}
+        locations.push(coords)
+      }
     }
   }
-  space[row] = arr
-  row++
+  return locations
 }
-console.log(space)
+
+const space = expandVertically(lines)
+const locs = galaxies(space)
+
+let sumOfPaths = 0
+while (locs.length > 1) {
+  for (let i = 1; i < locs.length; i++) {
+    const dist = locs[i].r - locs[0].r + Math.abs(locs[i].c - locs[0].c)
+    sumOfPaths += dist
+  }
+  locs.shift()
+}
+console.log('Part One:', sumOfPaths) // 9609130
+
+const locs2 = galaxies(space)
+
+const expansionFactor = 1000000
+const expandedLocs = locs2.map(loc => {
+  for (let i = emptyR.length - 1; i >= 0; i--) {
+    if (loc.r > (emptyR[i] + i)) {
+      loc.r += (expansionFactor - 2) * (i + 1)
+      break
+    }
+  }
+  for (let i = emptyC.length - 1; i >= 0; i--) {
+    if (loc.c > (emptyC[i] + i)) {
+      loc.c += (expansionFactor - 2) * (i + 1)
+      break
+    }
+  }
+  return loc
+})
+
+sumOfPaths = 0
+while (expandedLocs.length > 1) {
+  for (let i = 1; i < expandedLocs.length; i++) {
+    const dist = expandedLocs[i].r - expandedLocs[0].r + Math.abs(expandedLocs[i].c - expandedLocs[0].c)
+    sumOfPaths += dist
+  }
+  expandedLocs.shift()
+}
+console.log('Part Two:', sumOfPaths) // 702152204842
