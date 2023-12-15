9b66477d660860f8d693eecf92a9b44893859ca3
Matt Edwards <the.matt.edwards@gmail.com>
2023-12-15 12:40:02 +0000

    day 15 part two

--- day15/index.js
+++ day15/index.js
@@ -15,3 +15,59 @@ function convert(str) {
 const sumOfSteps = steps.reduce((tot, step) => tot = tot + convert(step), 0)
 
 console.log('Part One:', sumOfSteps) // 515210
+
+const boxes = new Array(256)
+for (let b = 0; b < boxes.length; b++) {
+  boxes[b] = []
+}
+
+function lensPos(box, label) {
+  for (let i = 0; i < boxes[box].length; i++) {
+    if (boxes[box][i].label === label) return i
+  }
+  return
+}
+
+function removeLens(box, label) {
+  const pos = lensPos(box, label)
+  if (pos !== undefined) {
+    boxes[box].splice(pos, 1)
+  }
+}
+
+function process(step) {
+  let label, fl, operator
+  if (step.includes('=')) {
+    operator = '='
+    label = step.split('=')[0]
+    fl = parseInt(step.split('=')[1])
+  } else {
+    operator = '-'
+    label = step.split('-')[0]
+  }
+  const box = convert(label)
+  if (operator === '-') {
+    removeLens(box, label)
+  } else {
+    if (lensPos(box, label) === undefined) {
+      boxes[box].push({label, fl})
+    } else {
+      const pos = lensPos(box, label)
+      boxes[box][pos].fl = fl
+    }
+  }
+}
+
+steps.forEach(step => process(step))
+
+let combinedPower = 0
+for (let b = 0; b < boxes.length; b++) {
+  let boxPower = 0
+  for (let l = 0; l < boxes[b].length; l++) {
+    let lensPower = (b + 1) * (l + 1) * boxes[b][l].fl
+    boxPower += lensPower
+  }
+  combinedPower += boxPower
+}
+
+console.log('Part Two:', combinedPower) // 246762
